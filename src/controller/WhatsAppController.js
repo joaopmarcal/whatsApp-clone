75ebcd3d477750cba9337118fb610092e07d51bb
joao Paulo <joaopaulozangrande@gmail.com>
2020-07-11 12:23:52 -0300

    reading file cover PDF and generating image with pdf

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -266,6 +266,10 @@ export class WhatsAppController {
 
             if (this.el.inputDocument.files.length){
 
+                this.el.panelDocumentPreview.css({
+                    'height':'1%'
+                });
+
                 let file = this.el.inputDocument.files[0];
 
                 this._documentPreviewController = new DocumentPreviewController(file);
@@ -277,7 +281,15 @@ export class WhatsAppController {
                     this.el.imagePanelDocumentPreview.show();
                     this.el.filePanelDocumentPreview.hide();
 
+                    this.el.panelDocumentPreview.css({
+                        'height':'calc(100% - 0px)'
+                    });
+
                 }).catch(err=>{
+
+                    this.el.panelDocumentPreview.css({
+                        'height':'calc(100% - 0px)'
+                    });
 
                     switch (file.type) {
 
