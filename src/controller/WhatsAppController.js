9392a6d7c62cd08b9a3e9804df0dbf7199fe64f1
joao Paulo <joaopaulozangrande@gmail.com>
2020-07-23 19:18:56 -0300

    Scrolling down automatically if it is leaning down

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -179,10 +179,14 @@ export class WhatsAppController {
             display:'flex'
         });
 
+        this.el.panelMessagesContainer.innerHTML = '';
+
         Message.getRef(this._contactActive.chatId).orderBy('timeStamp')
             .onSnapshot(docs => {
 
-                this.el.panelMessagesContainer.innerHTML = '';
+                let scrollTop = this.el.panelMessagesContainer.scrollTop;
+                let scrollTopMax = (this.el.panelMessagesContainer.scrollHeight - this.el.panelMessagesContainer.offsetHeight);
+                let autoScroll = (scrollTop >= scrollTopMax);
 
                 docs.forEach(doc => {
 
@@ -205,6 +209,16 @@ export class WhatsAppController {
                     }
 
                 });
+
+                if(autoScroll){
+
+                    this.el.panelMessagesContainer.scrollTop = (this.el.panelMessagesContainer.scrollHeight - this.el.panelMessagesContainer.offsetHeight);
+
+                } else {
+
+                    this.el.panelMessagesContainer.scrollTop = scrollTop;
+
+                }
 
             });
 
