196168cc007d4ea0581f18776e6202d895e46dde
joao Paulo <joaopaulozangrande@gmail.com>
2020-07-29 19:53:38 -0300

    send contact as attachment

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -7,6 +7,7 @@ import {User} from "./../model/User";
 import {Chat} from "../model/Chat";
 import {Message} from "../model/Message";
 import {Base64} from "../util/base64";
+import {ContactsController} from "../controller/ContactsController";
 
 export class WhatsAppController {
 
@@ -685,13 +686,25 @@ export class WhatsAppController {
 
         this.el.btnAttachContact.on('click', e=>{
 
-            this.el.modalContacts.show();
+            this._contactsController = new ContactsController(this.el.modalContacts, this._user);
+
+            this._contactsController.on('select', contact => {
+
+                Message.sendContact(
+                  this._contactActive.chatId,
+                  this._user.email,
+                  contact
+                );
+
+            });
+
+            this._contactsController.open();
 
         });
 
         this.el.btnCloseModalContacts.on('click', e=>{
 
-            this.el.modalContacts.hide();
+            this._contactsController.close();
 
         });
 
--- src/model/Message.js
+++ src/model/Message.js
@@ -52,6 +52,7 @@ export class Message extends Model{
 
         let div = document.createElement('div');
 
+        div.id = `_${this.id}`;
         div.className = 'message';
 
         switch (this.type) {
@@ -79,7 +80,7 @@ export class Message extends Model{
                                     </div>
                                 </div>
                                 <div class="_1lC8v">
-                                    <div dir="ltr" class="_3gkvk selectable-text invisible-space copyable-text">Nome do Contato Anexado</div>
+                                    <div dir="ltr" class="_3gkvk selectable-text invisible-space copyable-text">${this.content.name}</div>
                                 </div>
                                 <div class="_3a5-b">
                                     <div class="_1DZAH" role="button">
@@ -95,6 +96,21 @@ export class Message extends Model{
 
                     </div>
                 `;
+
+                if (this.content.photo){
+
+                    let img = div.querySelector('.photo-contact-sended');
+                    img.src = this.content.photo;
+                    img.show();
+
+                }
+
+                div.querySelector('.btn-message-send').on('click', e=>{
+
+                    console.log('Enviar Mensagem');
+
+                });
+
                 break;
 
             case 'image':
@@ -283,7 +299,7 @@ export class Message extends Model{
 
             default:
                 div.innerHTML = `
-                    <div class="font-style _3DFk6 tail" id="_${this.id}">
+                    <div class="font-style _3DFk6 tail">
                         <span class="tail-container"></span>
                         <span class="tail-container highlight"></span>
                         <div class="Tkt2p">
@@ -341,6 +357,12 @@ export class Message extends Model{
             });
 
         });
+
+    }
+
+    static sendContact(chatId, from, contact){
+
+        return Message.send(chatId, from, 'contact', contact);
 
     }
 
